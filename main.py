3a3c16577ffa1556c5b717661a5931831260cc5f
aryalarchit9 <aryalarchit9@gmail.com>
2026-02-22 17:09:37 +0545

    Integrated logging system with file-based execution tracking

--- main.py
+++ main.py
@@ -4,6 +4,13 @@ from steganography.embed import embed_message
 from steganography.extract import extract_message
 from steganalysis.histogram_analysis import compare_images
 import sys
+import logging
+
+logging.basicConfig(
+    filename="app.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 def main():
     print("=== Secure Steganographic Communication System ===")
@@ -20,28 +27,30 @@ def main():
     try:
         # Encrypt
         encrypted = encrypt_message(message, password)
+        logging.info("Message encrypted successfully.")
         print("\n[+] Message encrypted.")
 
         # Embed
         embed_message(input_image, output_image, encrypted)
+        logging.info("Message embedded successfully.")
         print("[+] Message embedded into image.")
 
         # Extract
         extracted_encrypted = extract_message(output_image)
+        logging.info("Message extracted successfully.")
         print("[+] Encrypted message extracted from image.")
 
         # Decrypt
         decrypted = decrypt_message(extracted_encrypted, password)
+        logging.info("Message decrypted successfully.")
         print("\n[+] Decrypted message:")
         print(decrypted)
 
         # Steganalysis
+        logging.info("Performing histogram analysis.")
         print("\n[+] Performing histogram analysis...")
         compare_images(input_image, output_image)
 
     except Exception as e:
+        logging.error(f"Error occurred: {str(e)}")
         print("\n[!] Error:", str(e))
-
-
-if __name__ == "__main__":
-    main()
